71aeb4dbaf10511219f3368a1073a7279af1ef2a
Cody Rude <cmrude@mit.edu>
2018-05-23 11:38:01 -0400

    Removed loading a file with scipy

--- pyinsar/data_import/import_srcmod.py
+++ pyinsar/data_import/import_srcmod.py
@@ -33,11 +33,10 @@ import re
 from scipy.io import loadmat
 from scipy.interpolate import SmoothBivariateSpline
 import numpy as np
-from pyproj import Proj
 
 from skdaccess.utilities.image_util import SplineGeolocation
 
-def read_srcmod_data(input_src_mat_filename):
+def read_srcmod_data(srcmod_data):
     '''
     Generate faults of okada sources from src mod mat files.
 
@@ -47,24 +46,9 @@ def read_srcmod_data(input_src_mat_filename):
     
     @return List of faults objects, list of slips, list of rakes
     '''
-    matdata = loadmat(input_src_mat_filename)
-
-    data_keys = []
-
-    # Find all non private keys
-    for key in matdata.keys():
-        if re.match('__', key) is None:
-            data_keys.append(key)
-
-    # Make sure there is only on data key
-    if len(data_keys) != 1:
-        raise ValueError('Too many keys in mat file')
-
-
-    data = matdata[data_keys[0]][0,0]
 
     # Number of segments
-    num_segments = data['invSEGM'][0,0].astype(int)
+    num_segments = int(srcmod_data['invSEGM'])
 
     if num_segments != 1:
         raise NotImplementedError('Only single segment srcmods are currently supported')
@@ -91,58 +75,55 @@ def read_srcmod_data(input_src_mat_filename):
     
 
 
-    lat = data['geoLAT']
-    lon = data['geoLON']
-
-    proj = Proj(proj='gnom', lat_0=lat, lon_0=lon)
+    lat = srcmod_data['geoLAT']
+    lon = srcmod_data['geoLON']
 
     # Size of the subfaults
-    Dz, Dx = data['invDzDx'][0,:]
+    Dz, Dx = srcmod_data['invDzDx']
 
     # Number of the subfaults
-    Nz, Nx = data['invNzNx'][0,:].astype(int)
+    Nz, Nx = srcmod_data['invNzNx'].astype(int)
 
     # Number of time windows
-    num_time_windows = data['invNoTW'].astype(int)
+    num_time_windows = int(srcmod_data['invNoTW'])
 
     def get_data_from_time_windows(data, prefix, alternate_column, num_windows, Nx, Nz, alternate_value=None):
-        if prefix + 'TW1' in data.dtype.names:
+        if prefix + 'TW1' in data.keys():
             new_data = np.zeros([num_time_windows, Nz, Nx])
 
             for num in range(1, num_windows+1):
                 new_data[num-1,:,:] = np.fliplr(np.flipud(data[prefix+'TW' + str(num)]))
 
-        elif alternate_column in data.dtype.names:
+        elif alternate_column in data.keys():
             new_data = np.fliplr(np.flipud(data[alternate_column]))
             new_data = new_data.reshape(1, *new_data.shape)
 
         else:
             new_data = np.ones([1, Nz, Nx])
-            new_data[0,:,:] = data[alternate_value][0,0]
+            new_data[0,:,:] = data[alternate_value]
 
         return new_data
 
 
-    fault_slip = get_data_from_time_windows(data, 'slip', 'slipSPL', num_time_windows, Nx, Nz) / 100
-    fault_rake = np.deg2rad(get_data_from_time_windows(data, 'rake', 'RAKE', num_time_windows, Nx, Nz, 'srcARake'))
+    fault_slip = get_data_from_time_windows(srcmod_data, 'slip', 'slipSPL', num_time_windows, Nx, Nz) / 100
+    fault_rake = np.deg2rad(get_data_from_time_windows(srcmod_data, 'rake', 'RAKE', num_time_windows, Nx, Nz, 'srcARake'))
 
 
     
 
     # Location of the hypocenter in the along strike/down dip
     # coordinate system
-    center_x, center_z = data['srcHypXZ'][0,:]
+    center_x, center_z = srcmod_data['srcHypXZ']
 
     # Distance from the surface to the top of the fault
-    z_top = data['srcZ2top'][0,0]
+    z_top = srcmod_data['srcZ2top']
 
     # Width and length of fault
-    width, length = data['srcDimWL'][0,:]
+    width, length = srcmod_data['srcDimWL']
 
     # Fault properties
-    fault_strike = np.deg2rad(data['srcAStke'][0,0])
-    fault_dip = np.deg2rad(data['srcDipAn'][0,0])
-
+    fault_strike = np.deg2rad(srcmod_data['srcAStke'])
+    fault_dip = np.deg2rad(srcmod_data['srcDipAn'])
 
     ### Generate fault ###
     # Hypocenter vector in the coordinate system where the centroid of
@@ -177,15 +158,12 @@ def read_srcmod_data(input_src_mat_filename):
     # Determine the centroids of each subfault that srcmod uses
     compare_with_provided_centers = translate(fault.cell_centroids[:,::-1]/1000, *compare_center_coords)
 
-    if not np.allclose(compare_with_provided_centers, np.stack([ data['geoX'].ravel(), 
-                                                                 data['geoY'].ravel(), 
-                                                                -data['geoZ'].ravel()])):
+    if not np.allclose(compare_with_provided_centers, np.stack([ srcmod_data['geoX'].ravel(),
+                                                                 srcmod_data['geoY'].ravel(),
+                                                                -srcmod_data['geoZ'].ravel()])):
 
         raise RuntimeError("Unable to recreate srcmod's centroids!")
 
     ### Finished generating fault ###
 
-    return [fault], [fault_slip], [fault_rake], proj
-
-    
-    
+    return [fault], [fault_slip], [fault_rake]
