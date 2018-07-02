3f3d3338fba01fbe5dd42d4773eb3fece6456220
Cody Rude <cmrude@mit.edu>
2018-07-02 11:29:00 -0400

    Add coherence and interferogram

--- pyinsar/processing/discovery/__init__.py
+++ pyinsar/processing/discovery/__init__.py
@@ -1,2 +1,4 @@
 from .deformation_to_phase import DeformationToPhase
 from .temporal_decorrelation import TemporalDecorrelation
+from .coherence import Coherence
+from .inteferogram import Interferogram
