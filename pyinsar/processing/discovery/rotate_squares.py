9e04b7713ad2ae8ba5f70bdafdef870594ade46e
Cody Rude <cmrude@mit.edu>
2018-07-13 15:32:11 -0400

    Added documentation

--- pyinsar/processing/discovery/rotate_squares.py
+++ pyinsar/processing/discovery/rotate_squares.py
@@ -1,3 +1,30 @@
+# The MIT License (MIT)
+# Copyright (c) 2018 Massachusetts Institute of Technology
+#
+# Authors: Cody Rude
+# This software is part of the NSF DIBBS Project "An Infrastructure for
+# Computer Aided Discovery in Geoscience" (PI: V. Pankratius) and
+# NASA AIST Project "Computer-Aided Discovery of Earth Surface
+# Deformation Phenomena" (PI: V. Pankratius)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 from collections import OrderedDict
 
 from skdiscovery.data_structure.framework.base import PipelineItem
@@ -9,6 +36,15 @@ import scipy as sp
 from pyinsar.processing.utilities import insar_simulator_utils
 
 def rotateSquare(image, square, angle, order):
+    '''
+    Rotate a subsection of an image defined by a shapely square
+
+    @param image: Full image containing subsection to be rotated
+    @param square: Shapely square
+    @param angle: Angle of rotation
+    @param order: Order of spline interpolation
+
+    '''
     x_start, y_start, x_end, y_end = np.rint(square.bounds).astype(np.int)
     size = x_end - x_start
 
@@ -21,7 +57,20 @@ def rotateSquare(image, square, angle, order):
 
 
 class RotateSquares(PipelineItem):
+    '''
+    Generate new images by rotating subsections of data defined by Shapely squares
+    '''
+
     def __init__(self, str_description, ap_paramList, square_result_name, angles, clean=True):
+        '''
+        Initialize RotateSquares object
+
+        @param str_description: String describing class
+        @param ap_param[SplineOrder]: Spline order used in interpolation
+        @param square_result_name: Name of pipeline item that contains the Shapely squares
+        @param angles: Angles used when rotating squares
+        @param clean: Remove any squares that contain NaN's
+        '''
 
         self._angles = angles
 
@@ -33,6 +82,11 @@ class RotateSquares(PipelineItem):
 
 
     def process(self, obj_data):
+        '''
+        Generate rotated images based on Shapely squares
+
+        @param obj_data: Image data wrapper
+        '''
 
         data_result = OrderedDict()
         meta_result = OrderedDict()
