428f9a5e4c2d6d62ff12633e9f53a8f1a9ea43db
Cody Rude <cmrude@mit.edu>
2018-07-16 09:37:33 -0400

    Add wrap phase pipeline item

--- pyinsar/processing/discovery/__init__.py
+++ pyinsar/processing/discovery/__init__.py
@@ -2,3 +2,4 @@ from .deformation_to_phase import DeformationToPhase
 from .temporal_decorrelation import TemporalDecorrelation
 from .coherence import Coherence
 from .inteferogram import Interferogram
+from .wrap_phase import WrapPhase
