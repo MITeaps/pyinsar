9d28d86db6018f5a87f4e5de547f7be47f098dc6
Guillaume Rongier <grongier>
2018-07-24 14:19:29 -0400

    Added ISCE processing functions

--- pyinsar/data_import/sentinel.py
+++ pyinsar/data_import/sentinel.py
@@ -1,7 +1,7 @@
 # The MIT License (MIT)
 # Copyright (c) 2018 Massachusetts Institute of Technology
 #
-# Author: Cody Rude
+# Author: Cody Rude, Guillaume Rongier
 # This software has been created in projects supported by the US National
 # Science Foundation and NASA (PI: Pankratius)
 #
@@ -23,10 +23,17 @@
 # THE SOFTWARE.
 
 # Standard library imports
+import requests
+from getpass import getpass
+from glob import glob
 import xml.etree.ElementTree as ET
 
 # 3rd party imports
 import pandas as pd
+import geopandas as gpd
+from lxml import html
+
+from pyinsar.data_import.import_utils import *
 
 def parse_satellite_data(in_satellite_file):
     '''
@@ -51,3 +58,96 @@ def parse_satellite_data(in_satellite_file):
         tmp_data.append(row)
 
     return pd.DataFrame(tmp_data, columns=names)
+
+def get_url_precise_orbit(product_name):
+    '''
+    Get the URL of the precise orbit corresponding to a given Sentinel-1 product
+    Modified from https://github.com/scottyhq/dinoSARaws/blob/48f68b0c49b26a91b501bc6d3fb1b2eb4c6c3918/bin/prep_topsApp_aws.py
+    
+    @param product_name: Name of the Sentinel-1 product
+    
+    @return The URL is the precise orbit file exists, none otherwise
+    '''
+    satellite = product_name[:3]
+    date = product_name[17:25]
+    # Incomplete inventory: https://s1qc.asf.alaska.edu/aux_poeorb/files.txt
+    base_url = 'https://s1qc.asf.alaska.edu/aux_poeorb'
+    r = requests.get(base_url)
+    webpage = html.fromstring(r.content)
+    orbits = webpage.xpath('//a/@href')
+    # Get S1A or S1B
+    df = gpd.pd.DataFrame(dict(orbit=orbits))
+    df_sat = df[df.orbit.str.startswith(satellite)].copy()
+    day_before = gpd.pd.to_datetime(date) - gpd.pd.to_timedelta(1, unit = 'd')
+    day_before = day_before.strftime('%Y%m%d')
+    # Get matching orbit file
+    df_sat.loc[:, 'start_time'] = df_sat.orbit.str[42:50]
+    match = df_sat.loc[df_sat.start_time == day_before, 'orbit'].values
+    
+    if len(match) == 0:
+        print('No Sentinel-1 precise orbit for the product', product_name)
+        return None
+    return f'{base_url}/{match[0]}'
+                
+def download_precise_orbits(product_folder, orbit_folder, username, password):
+    '''
+    Download the precise orbits for all the Sentinel-1 products in a folder
+    
+    @param product_folder: The folder where the Sentinel-1 products are
+    @param orbit_folder: The folder where to put the orbit files
+    @param username: The username for authentification on Earthdata
+    @param password: The password for authentification on Earthdata
+    
+    @return The paths of the orbit files, none if a file couldnot be downloaded
+    '''
+    product_paths = glob(product_folder + '/*.zip')
+    precise_orbit_paths = []
+    for product_path in product_paths:
+        product_name = product_path.split('/')[-1].split('.')[0]
+        precise_orbit_url = get_url_precise_orbit(product_name)
+        if precise_orbit_url is not None:
+            precise_orbit_paths.append(download_file(precise_orbit_url,
+                                                     orbit_folder,
+                                                     username,
+                                                     password))
+    
+    return precise_orbit_paths
+
+def download_products(product_names,
+                      product_folder,
+                      base_url = 'https://datapool.asf.alaska.edu/SLC',
+                      use_vertex = True,
+                      username = None,
+                      password = None):
+    '''
+    Download Sentinel-1 products in a folder
+    
+    @param product_names: List of Sentinel-1 product names
+    @param product_folder: The folder where to put the product files
+    @param base_url: Base url from where to download the files (default is from 
+                     the Alaska Satellite Facility)
+    @param use_vertex: True if the base url is that of the Alaska Satellite Facility
+    @param username: The username for authentification on Earthdata
+    @param password: The password for authentification on Earthdata
+    
+    @return The paths of the orbit files, none if a file couldnot be downloaded
+    '''
+    if base_url[-1] != '/':
+        base_url += '/'
+    
+    product_paths = []
+    for i, product_name in enumerate(product_names):
+        print('Downloading file ', i + 1, '/', len(product_names), sep = '', end = '\r')
+        satellite = ''
+        if product_name[0:3] == 'S1B' and use_vertex == True:
+            satellite = 'SB/'
+        elif product_name[0:3] == 'S1A' and use_vertex == True:
+            satellite = 'SA/'
+        product_url = base_url + satellite + product_name + '.zip'
+        product_paths.append(download_file(product_url,
+                                           product_folder,
+                                           username,
+                                           password))
+    print('\033[K\033[K\033[K\033[K\033[K\033[K\033[KDownload over')
+    
+    return product_paths
--- pyinsar/output/plot_raster.py
+++ pyinsar/output/plot_raster.py
@@ -26,7 +26,7 @@ import numpy as np
 
 from numba import jit
 import matplotlib.pyplot as plt
-from ipywidgets import interact, BoundedIntText
+from ipywidgets import interact, interactive, BoundedIntText, Dropdown, HBox
 
 def average_minmax_slices(array, axis = 0):
     '''
@@ -74,6 +74,9 @@ def plot_interactive_slicing(array,
     @param clabel: A label for the colorbar
     @param xlabel: A label for the x axis
     @param ylabel: A label for the y axis
+    @param figsize: A 2D tuple for the figure size
+    @param update_colorbar: True if the colorbar must be updated between each
+                            slice, false otherwise
     '''
     s = [slice(slice_index, slice_index + 1) if i == axis else slice(None) for i in range(3)]
     sliced_array = array[s].squeeze()
@@ -149,3 +152,124 @@ def plot_interactive_slicing(array,
                                                                  continuous_update = False))
 
     plt.draw()
+
+def plot_interactive_multiple_slicing(array,
+                                      axes,
+                                      slice_indexes,
+                                      model_array = None,
+                                      cmap = 'viridis',
+                                      update_colorbar = False,
+                                      vmin = 0.,
+                                      vmax = 1.,
+                                      extent = None,
+                                      clabel = '',
+                                      xlabel = '',
+                                      ylabel = '',
+                                      figsize = None):
+    '''
+    Plot 2D slices of a ND NumPy array, with the possibility to loop through and
+    visualize the slices
+    
+    @param array: A ND NumPy array
+    @param axes: A list of N - 2 elements that contains the indexes of the axes 
+                 to slice
+    @param slice_indexes: A list of N - 2 elements that contains the initial 
+                          slice to show for each axis
+    @param model_array: A 2D NumPy array to compare to the slices
+    @param cmap: A colormap for the array
+    @param update_colorbar: True if the colorbar must be updated between each 
+                            slice, false otherwise
+    @param v_min: The minimal value of the colorbar if update_colorbar if false 
+                  and no model is used
+    @param v_max: The maximal value of the colorbar if update_colorbar if false 
+                  and no model is used
+    @param extent: The spatial extent of the slice
+    @param clabel: A label for the colorbar
+    @param xlabel: A label for the x axis
+    @param ylabel: A label for the y axis
+    @param figsize: A 2D tuple for the figure size
+    '''
+    assert len(set(axes)) == len(axes), "Axes must be unique"
+    assert len(slice_indexes) == len(axes), "Slice indexes and axes should have the same length"
+    assert array.ndim - len(axes) == 2, "The plotted array should be two-dimensional"
+    
+    s = [slice(None) for i in range(array.ndim)]
+    for axis, slice_index in zip(axes, slice_indexes):
+        s[axis] = slice(slice_index, slice_index + 1)
+    sliced_array = array[s].squeeze()
+    
+    if update_colorbar == False:
+        if model_array is not None:
+            vmin = np.nanmin(model_array)
+            vmax = np.nanmax(model_array)
+    else:
+        vmin = np.nanmin(sliced_array)
+        vmax = np.nanmax(sliced_array)
+    
+    number_plots = 1
+    if model_array is not None:
+        number_plots = 2
+    
+    figure, subplots = plt.subplots(1,
+                                    number_plots,
+                                    sharex = True, 
+                                    sharey = True, 
+                                    figsize = figsize)
+     
+    subplot = subplots
+    if model_array is not None:
+        subplot = subplots[1]
+        
+        model_raster_map = subplots[0].imshow(np.ma.masked_invalid(model_array), 
+                                              extent = extent,
+                                              cmap = cmap, interpolation = 'None',
+                                              rasterized = True,
+                                              vmin = vmin,
+                                              vmax = vmax,
+                                              zorder = 0)
+        subplots[0].set_xlabel(xlabel)
+        subplots[0].set_ylabel(ylabel)
+
+    raster_map = subplot.imshow(np.ma.masked_invalid(sliced_array),
+                                extent = extent,
+                                cmap = cmap, interpolation = 'None',
+                                rasterized = True,
+                                vmin = vmin,
+                                vmax = vmax,
+                                zorder = 0)
+    raster_map_colorbar = plt.colorbar(raster_map, ax = subplots)
+    raster_map_colorbar.set_label(clabel)
+    if model_array is None:
+        subplot.set_ylabel(ylabel)
+    subplot.set_xlabel(xlabel)
+
+    plt.show()
+    
+    def update_imshow(new_axis, new_slice_index):
+        s[new_axis] = slice(new_slice_index, new_slice_index + 1)
+        sliced_array = array[s].squeeze()
+        raster_map.set_data(sliced_array)
+        if update_colorbar == True:
+            raster_map.set_clim(vmin = np.nanmin(sliced_array),
+                                vmax = np.nanmax(sliced_array))
+            if model_array is not None:
+                model_raster_map.set_clim(vmin = np.nanmin(sliced_array),
+                                          vmax = np.nanmax(sliced_array))
+
+
+    for axis, slice_index in zip(axes, slice_indexes):
+        axis_widget = Dropdown(options = [axis],
+                               value = axis,
+                               description = 'Axis',
+                               disabled = False)
+        slice_widget = BoundedIntText(value = slice_index,
+                                      min = 0,
+                                      max = array.shape[axis] - 1,
+                                      step = 1,
+                                      description = 'Slice',
+                                      continuous_update = False)
+        widget = interactive(update_imshow,
+                             new_axis = axis_widget,
+                             new_slice_index = slice_widget)
+        box = HBox(widget.children)
+        display(box)
