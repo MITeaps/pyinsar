3b9b8b74141a6bef96f85ef744632841f1cc8404
Cody Rude <cmrude@mit.edu>
2018-08-16 09:47:31 -0400

    Making hdf_retriever more generic

--- pyinsar/processing/data_fetcher/hdf_retriever.py
+++ pyinsar/processing/data_fetcher/hdf_retriever.py
@@ -42,7 +42,7 @@ class DataFetcher(DataFetcherBase):
     Data fetcher for retrieving hdf image data made for training in convolutional neural networks
     """
 
-    def __init__(self, filename_list, label_list, size, dtype, num_chunks, num_training_items, num_validation_items, num_testing_items):
+    def __init__(self, filename_list, label_list, size, dtype, num_chunks, index):
         """
         Initialize TrainCNN item
 
@@ -61,74 +61,18 @@ class DataFetcher(DataFetcherBase):
         self._size = size
         self._dtype = dtype
         self._num_chunks = num_chunks
-        self._num_training_items = num_training_items
-        self._num_validation_items = num_validation_items
-        self._num_testing_items = num_testing_items
+        self._index = index
         
         self._current_chunk = 0
         
         super(DataFetcher, self).__init__()
 
-        assert self._num_training_items % self._num_chunks == 0, "Number of training items must be divisible by number of chunks"
-        self._items_per_chunk = (self._num_training_items * len(self._label_list)) // self._num_chunks
+        assert self._index.shape[0] % self._num_chunks == 0, "Number of training items must be divisible by number of chunks"
+        self._items_per_chunk = self._index.shape[0] // self._num_chunks
 
         data_retriever = DataRetriever(self._filename_list, self._label_list, self._size, self._dtype)
         num_images = data_retriever.get_num_images()
         num_labels = len(self._label_list)
-
-        # self._full_index = np.zeros((np.sum(num_images[:,1]), 2), dtype=np.int)
-
-        # start_image_index = 0
-        # for num_images_index, label in enumerate(self._label_list):
-        #     end_image_index = start_image_index + num_images[num_images_index, 1]
-        #     self._full_index[start_image_index:end_image_index, 0] = label
-        #     self._full_index[start_image_index:end_image_index, 1] = np.arange(end_image_index-start_image_index)
-        #     start_image_index = end_image_index
-
-
-
-        self._training_index = np.zeros((num_labels * self._num_training_items, 2), dtype = np.int)
-        self._validation_index = np.zeros((num_labels * self._num_validation_items, 2), dtype = np.int)
-        self._testing_index = np.zeros((num_labels * self._num_testing_items, 2), dtype = np.int)
-
-
-        def add_indices(index_array, image_index, label_index, label, num_items):
-            """
-            Add indices to an index array
-
-            @param index_array: Array being used as the index
-            @param image_index: Indices of imaging to insert into the index array
-            @param label_index: Index of the current label
-            @param label: Label of the current image index
-            """
-            my_slice = subarray_slice(label_index, num_items)
-            index_array[my_slice, 0] = label
-            index_array[my_slice, 1] = image_index
-
-        def image_index_slice(num_skip_items, num_items):
-            """
-            Select num_items from an array after skipping num_skip_items
-
-            @param num_skip_items: Number of items to skip
-            @param num_items: Number of items to select
-            @return slice that starts after num_skip_items and is num_items long
-            """
-            return slice(num_skip_items, num_items + num_skip_items)
-
-
-        for label_index, label in enumerate(self._label_list):
-            image_index = np.arange(num_images[label_index, 1])
-            np.random.shuffle(image_index)
-
-            training_image_slice = image_index_slice(0, self._num_training_items)
-            validation_image_slice = image_index_slice(self._num_training_items, self._num_validation_items)
-            testing_image_slice = image_index_slice(self._num_training_items + self._num_validation_items, self._num_testing_items)
-
-            add_indices(self._training_index, image_index[training_image_slice], label_index, label, self._num_training_items)
-            add_indices(self._validation_index, image_index[validation_image_slice], label_index, label, self._num_validation_items)
-            add_indices(self._testing_index, image_index[testing_image_slice], label_index, label, self._num_testing_items)
-
-        np.random.shuffle(self._training_index)
 
     def perturb(self):
         self._current_chunk = (self._current_chunk + 1) % self._num_chunks
@@ -144,11 +88,11 @@ class DataFetcher(DataFetcherBase):
 
         data_slice = subarray_slice(self._current_chunk, self._items_per_chunk)
 
-        data[data_label] = data_retriever.get_images(self._training_index[data_slice,:])
+        data[data_label] = data_retriever.get_images(self._index[data_slice,:])
         metadata[data_label] = OrderedDict()
         metadata[data_label]['Num_Chunks'] = self._num_chunks
         metadata[data_label]['Current_Chunk'] = self._current_chunk
-        metadata[data_label]['Labels'] = self._training_index[data_slice, 0]
+        metadata[data_label]['Labels'] = self._index[data_slice, 0]
 
         return ImageWrapper(data, -1, metadata)
 
